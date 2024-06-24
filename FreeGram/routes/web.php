0959ca041e11b08eee4aab6cb37419b54c115a44
loveass444 <cornellzzzzzz@gmail.com>
2024-06-24 21:53:50 +0700

    menghubungkan button front end dan backend lalu membuat route untuk post

--- FreeGram/routes/web.php
+++ FreeGram/routes/web.php
@@ -2,11 +2,16 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
+
+Route::resource('posts', PostController::class);
+Route::get('/dashboard', [PostController::class, 'index'])->name('dashboard');
 
 Route::get('/', function () {
     return view('login');
 });
-
+Route::resource('posts', PostController::class);
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -15,6 +20,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
 });
+
+Route::resource('posts', PostController::class);
+Route::get('/dashboard', [PostController::class, 'index'])->name('dashboard');
+
+
 
 require __DIR__.'/auth.php';
